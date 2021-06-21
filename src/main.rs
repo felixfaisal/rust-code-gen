c333302fcc17575563ee0a0acfe4e86eff2836c7
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-06-21 15:10:02 +0530

    Tested out with a few hardcoded ip4 addresses

--- src/main.rs
+++ src/main.rs
@@ -31,12 +31,12 @@ impl Felix {
 }
 #[derive(Debug, Clone, Copy)]
 struct union {
-    XXX_unionData: [u8; 8],
+    XXX_unionData: [u8; 16],
 }
 impl union {
     pub fn new() -> union {
         union {
-            XXX_unionData: [192, 168, 225, 0, 0, 0, 0, 0],
+            XXX_unionData: [0; 16],
         }
     }
     pub fn write(&mut self) {
@@ -49,17 +49,32 @@ impl union {
             self.XXX_unionData[i] = 70;
         }
     }
+    pub fn SetIP4(&mut self) {
+        println!("Setting IP4");
+        for i in 0..4 {
+            match i {
+                0 => self.XXX_unionData[i] = 192,
+                1 => self.XXX_unionData[i] = 226,
+                2 => self.XXX_unionData[i] = 120,
+                3 => self.XXX_unionData[i] = 20,
+                _ => println!("Reached end of the array"),
+            }
+        }
+    }
     pub fn GetIP4(&self) {
+        println!("Fetching IP4");
         for i in 0..4 {
             println!("{}", self.XXX_unionData[i]);
         }
     }
     pub fn SetIP6(&mut self) {
+        println!("Setting IP6");
         for i in 0..self.XXX_unionData.len() {
-            self.XXX_unionData[i] = 0
+            self.XXX_unionData[i] = 10;
         }
     }
     pub fn GetIP6(&self) {
+        println!("Fetching IP6");
         for i in 0..self.XXX_unionData.len() {
             println!("{}", self.XXX_unionData[i]);
         }
@@ -72,11 +87,12 @@ fn main() {
     // run_shell();
     let mut uniondata = union::new();
     dbg!(uniondata.XXX_unionData);
-    uniondata.write();
+    // uniondata.write();
     // uniondata.XXX_unionData[1] = 0;
     //uniondata.write();
     // uniondata.update();
     // uniondata.write();
+    uniondata.SetIP4();
     uniondata.GetIP4();
     uniondata.SetIP6();
     uniondata.GetIP6();
